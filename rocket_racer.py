d4077910336fd3443e15e547d153b28a4c5fcfdf
Carlos <62441132+carlosoliva2000@users.noreply.github.com>
2022-02-22 14:46:58 +0100

    Generación de nivel

--- rocket_racer.py
+++ rocket_racer.py
@@ -8,13 +8,15 @@ from pygame import gfxdraw
 class Entorno:
     COLOR = (50, 50, 50)
 
-    def __init__(self):
+    def __init__(self, pos_inicial=(0, 0)):
         """
         Llama a la función inicializar.
         """
+        self.recorrido = None
         self.bordes1 = None
         self.bordes2 = None
         self.checkpoints = None
+        self.pos_inicial = pos_inicial
         self.inicializar()
 
     def inicializar(self):
@@ -27,13 +29,130 @@ class Entorno:
         """
         Genera el nivel aleatorio, primero generando segmentos y luego aplicándoles anchura, y checkpoints.
         """
-        pass
+        longitud_min = 60
+        longitud_max = 150
+        d_ang = 0.5  # 0.8
+        anchura_min = 50
+        anchura_max = 150
+        d_anchura = 5
+        segmentos = 50
 
-    def mover(self):
+        # Recorrido es una matriz de 3 columnas que representan:
+        # - [0] = x
+        # - [1] = y
+        # - [2] = ang
+        recorrido = np.zeros((segmentos, 3))
+        bordes1 = np.zeros((segmentos, 2))
+        bordes2 = np.zeros((segmentos, 2))
+        anchura = (anchura_min + anchura_max) // 2
+
+        # Ajustamos los valores iniciales del recorrido
+        # Es necesario empezar ligeramente a la derecha del cohete, ya que al final añadiremos dos puntos a cada lista
+        # para cerrar el recorrido por la izquierda
+        recorrido[0, 0] = self.pos_inicial[0] + 50
+        recorrido[0, 1] = self.pos_inicial[1]
+        bordes1[0, 0] = self.pos_inicial[0] + 50
+        bordes1[0, 1] = self.pos_inicial[1]
+        bordes2[0, 0] = self.pos_inicial[0] + 50
+        bordes2[0, 1] = self.pos_inicial[1]
+
+        for i in range(1, segmentos):
+            # Longitud del segmento actual
+            longitud = np.random.randint(longitud_min, longitud_max)
+
+            # Sentido del giro del segmento
+            signo = 1 if np.random.rand() > 0.5 else -1
+
+            # Ángulo del nuevo segmento en función del anterior
+            angulo = recorrido[i - 1, 2] + signo * np.random.rand() * d_ang
+            print(angulo)
+            if angulo > np.pi:
+                angulo -= 2 * np.pi
+            if angulo < -np.pi:
+                angulo += 2 * np.pi
+
+            # Calculamos los puntos de destino del nuevo segmento
+            x_ant, y_ant = recorrido[i - 1, 0], recorrido[i - 1, 1]
+            x = x_ant + longitud * np.cos(angulo)
+            y = y_ant + longitud * np.sin(angulo)
+
+            # Comprobamos si existe colisión con el segmento anterior
+            # En caso de producirse, retrocedemos N pasos o iteraciones
+            ############################################################
+
+            # Guardamos el recorrido en la matriz
+            recorrido[i, 0] = x
+            recorrido[i, 1] = y
+            recorrido[i, 2] = angulo
+
+            # Establece si la anchura crece o decrece en el segmento
+            # signo = 1 if np.random.rand() > 0.5 else -1
+
+            # Generamos anchura para el recorrido y sus respectivos bordes
+            # anchura = np.clip(anchura_ant + signo * np.random.rand() * d_anchura, anchura_min, anchura_max)
+            # print(anchura)
+            # bordes1[i, 0] = x + anchura * np.cos(angulo + 0.5 * np.pi)
+            # bordes1[i, 1] = y + anchura * np.sin(angulo + 0.5 * np.pi)
+            # bordes2[i, 0] = x - anchura * np.cos(angulo + 0.5 * np.pi)
+            # bordes2[i, 1] = y - anchura * np.sin(angulo + 0.5 * np.pi)
+
+        i = 0
+        while i < segmentos:
+            # Establece si la anchura crece o decrece en el segmento
+            if anchura == anchura_max:
+                signo = -1
+            elif anchura == anchura_min:
+                signo = 1
+            else:
+                signo = 1 if np.random.rand() > 0.5 else -1
+
+            j = 0
+            while i < segmentos and j < np.random.randint(5, 15):
+                # Generamos anchura para el recorrido en función de la anterior y lo limitamos
+                anchura = np.clip(anchura + signo * np.random.rand() * d_anchura, anchura_min, anchura_max)
+                # print(anchura)
+
+                # Definimos los bordes a partir del recorrido y su anchura
+                # Los nuevos puntos se definen tomando la perpendicular del segmento
+                x = recorrido[i, 0]
+                y = recorrido[i, 1]
+                angulo = recorrido[i, 2]
+                bordes1[i, 0] = x + anchura * np.cos(angulo + 0.5 * np.pi)
+                bordes1[i, 1] = y + anchura * np.sin(angulo + 0.5 * np.pi)
+                bordes2[i, 0] = x - anchura * np.cos(angulo + 0.5 * np.pi)
+                bordes2[i, 1] = y - anchura * np.sin(angulo + 0.5 * np.pi)
+
+                # Aumentamos contadores
+                j += 1
+                i += 1
+
+                if anchura == anchura_max or anchura == anchura_min:
+                    break
+
+        # Ajustes para comenzar siempre hacia la derecha y recto
+        borde1_inicio = np.array([[self.pos_inicial[0] - 100, self.pos_inicial[1]],
+                                  [self.pos_inicial[0] - 50, self.pos_inicial[1] + 100]])
+        borde2_inicio = np.array([[self.pos_inicial[0] - 100, self.pos_inicial[1]],
+                                  [self.pos_inicial[0] - 50, self.pos_inicial[1] - 100]])
+        recorrido_inicio = np.array([[self.pos_inicial[0] - 100, self.pos_inicial[1]],
+                                     [self.pos_inicial[0] - 50, self.pos_inicial[1]]])
+        # Concatenamos los inicios a las listas, y en el caso de recorrido, recortamos las columnas
+        # para quedarnos solo con las posiciones x e y
+        recorrido = np.concatenate([recorrido_inicio, recorrido[:, :2]])
+        bordes1 = np.concatenate([borde1_inicio, bordes1], axis=0)
+        bordes2 = np.concatenate([borde2_inicio, bordes2], axis=0)
+
+        self.recorrido = recorrido #[:, :2]
+        self.bordes1 = bordes1
+        self.bordes2 = bordes2
+
+    def mover(self, dx, dy):
         """
         Desplaza los elementos del entorno para que el jugador permanezca centrado.
         """
-        pass
+        self.recorrido -= (dx, dy)
+        self.bordes1 -= (dx, dy)
+        self.bordes2 -= (dx, dy)
 
     def obtener_checkpoint_sig(self):
         """
@@ -55,6 +174,21 @@ class Entorno:
         """
         ventana.fill(self.COLOR)
 
+        for i in range(len(self.recorrido) - 1):
+            # Checkpoints
+            pygame.draw.line(ventana, (80, 80, 80), self.bordes1[i], self.bordes2[i], 3)
+
+            # Carril central del recorrido
+            pygame.draw.line(ventana, (122, 122, 122), self.recorrido[i], self.recorrido[i + 1], 3)
+            pygame.draw.circle(ventana, (200, 200, 200), self.recorrido[i + 1], 5, 0)
+
+            # Bordes del recorrido
+            pygame.draw.line(ventana, (255, 0, 0), self.bordes1[i], self.bordes1[i + 1], 3)
+            pygame.draw.circle(ventana, (122, 0, 0), self.bordes1[i + 1], 5, 0)
+
+            pygame.draw.line(ventana, (0, 0, 255), self.bordes2[i], self.bordes2[i + 1], 3)
+            pygame.draw.circle(ventana, (0, 0, 122), self.bordes2[i + 1], 5, 0)
+
 
 class Cohete:
     # VEL_MAX = 5
@@ -62,16 +196,17 @@ class Cohete:
     ACELERACION = 0.5  # Aceleración lineal
     RAYOS = 7  # Número de rayos para realizar el raycast
     COLOR = (255, 255, 255)
+    RADIO = 10
 
-    def __init__(self, entorno, x=0, y=0):
+    def __init__(self, entorno, pos_inicial=(0, 0)):
         """
         Define el entorno y llama a inicializar.
         """
         self.entorno = entorno
 
         # Posición y orientación
-        self.x = x
-        self.y = y
+        self.x = pos_inicial[0]
+        self.y = pos_inicial[1]
         self.ang = 0  # Ángulo en radianes
 
         # Velocidad
@@ -131,8 +266,12 @@ class Cohete:
         self.rotar(accion[1])
         self.acelerar(accion[0])
 
-        self.x += self.dx
-        self.y += self.dy
+        # El cohete avanza en función de su velocidad
+        # self.x += self.dx
+        # self.y += self.dy
+
+        # Si queremos al cohete fijo en pantalla, entonces movemos el entorno
+        self.entorno.mover(self.dx, self.dy)
 
         # --------------------- VECTOR MOVIMIENTO -----------------------
 
@@ -174,10 +313,14 @@ class Cohete:
         """
         Renderiza el cohete
         """
-        pygame.gfxdraw.aacircle(ventana, int(self.x), int(self.y), 5, self.COLOR)
-        pygame.gfxdraw.filled_circle(ventana, int(self.x), int(self.y), 5, self.COLOR)
-        pygame.draw.aaline(ventana, self.COLOR, (self.x, self.y), (self.x + np.cos(self.ang) * 10,
-                                                                   self.y + np.sin(self.ang) * 10))
+        # pygame.draw.circle(ventana, self.COLOR, (self.x, self.y), 5.0)
+        # pygame.draw.line(ventana, self.COLOR, (self.x, self.y), (self.x + np.cos(self.ang) * 10,
+        #                                                         self.y + np.sin(self.ang) * 10))
+
+        pygame.gfxdraw.aacircle(ventana, int(self.x), int(self.y), self.RADIO, self.COLOR)
+        pygame.gfxdraw.filled_circle(ventana, int(self.x), int(self.y), self.RADIO, self.COLOR)
+        pygame.draw.aaline(ventana, self.COLOR, (self.x, self.y), (self.x + np.cos(self.ang) * 2 * self.RADIO,
+                                                                   self.y + np.sin(self.ang) * 2 * self.RADIO))
 
 
 class Juego(gym.Env):
@@ -189,17 +332,19 @@ class Juego(gym.Env):
         self.action_space = []
         self.observation_space = []
 
-        self.entorno = Entorno()
-        self.cohete = Cohete(self.entorno, 400, 300)
-
         # Atributos realizados con PyGame
         if not render:
             print("WARNING: Las opciones de renderizado no han sido activadas. Llamar a render() lanzará un error.\n"
                   "         Para activarlo, llame a inicializar_render().")
             self.clock = None
             self.ventana = None
+            self.ancho = 0
+            self.alto = 0
         else:
             self.inicializar_render()
+
+        self.entorno = Entorno((self.ancho // 2, self.alto // 2))
+        self.cohete = Cohete(self.entorno, (self.ancho // 2, self.alto // 2))
 
     def step(self, action):
         """
@@ -232,7 +377,9 @@ class Juego(gym.Env):
         # Actualizamos la pantalla con el frame generado
         pygame.display.update()
 
-    def inicializar_render(self):
+    def inicializar_render(self, ancho=800, alto=600):
         pygame.init()
         self.clock = pygame.time.Clock()
-        self.ventana = pygame.display.set_mode((800, 600))
+        self.ventana = pygame.display.set_mode((ancho, alto))
+        self.ancho = ancho
+        self.alto = alto
